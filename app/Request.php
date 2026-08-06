9d90a3861b2ffcad54e2ffb88eb6908979e16928
Pantea Marius-ciclistu <macropay-solutions@users.noreply.github.com>
2026-08-06 11:01:11 +0300

    Move getFiltered forceReplace and forceOffsetUnset in base Request.php

--- app/Request.php
+++ app/Request.php
@@ -2,11 +2,6 @@
 
 namespace App;
 
-/**
- * @method getFiltered(string $key, mixed $default = null, string|int $filter = FILTER_DEFAULT, int|array $options = [])
- * @method Request forceReplace(array $data)
- * @method Request forceOffsetUnset(string $offset)
- */
 class Request extends \MacropaySolutions\Framework\Http\Request
 {
     use RequestTrait;
